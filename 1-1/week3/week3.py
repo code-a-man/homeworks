8f70548c0525f88495fd6fbb9615c756b2e79291
Code a Man <43219246+code-a-man@users.noreply.github.com>
2022-02-21 22:35:28 +0300

    :pencil2: typo

--- 1-1/week3/week3.py
+++ 1-1/week3/week3.py
@@ -2,8 +2,8 @@ if __name__ == "__main__":
     name, surname, no = input("Name: "), input("Surname: "), int(input("No : "))
     birthday = input("Enter your birthday as dd/mm/yyyy: ").split("/")
     day, month, year = int(birthday[0]), int(birthday[1]), int(birthday[2])
-    x = (day ** month)**(1/2)  # 2
-    x = x / no  # 3
-    x = x * year  # a4
+    x = (day ** month)**(1/2)  # step 2
+    x = x / no  # step 3
+    x = x * year  # step 4
     space = " " * day  # space for the number of days
     print(f"{name} {space} {surname}, {no**2}, {format(x, '.5f')}")
